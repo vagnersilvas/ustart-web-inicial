55393d976f55e8a0456c6c760c9d016a1de8d38d
vagner da silva <vagnrdaia@gmail.com>
2021-11-17 21:28:57 -0300

    create component cad imovel

---
+++ src/app/imovel/pages/cad-imovel/cad-imovel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cad-imovel',
+  templateUrl: './cad-imovel.component.html',
+  styleUrls: ['./cad-imovel.component.scss']
+})
+export class CadImovelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
